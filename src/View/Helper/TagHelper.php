25749c71f47c2bc64ceec89e786b85b69f240fae
Graham Watson <graham@phantomwatson.com>
2017-09-18 20:04:55 -0400

    Fix formatting

--- src/View/Helper/TagHelper.php
+++ src/View/Helper/TagHelper.php
@@ -8,63 +8,64 @@ class TagHelper extends Helper
 {
     public $helpers = ['Html', 'Js'];
 
-    private function availableTagsForJs($available_tags)
+    private function availableTagsForJs($availableTags)
     {
-        $array_for_json = [];
-        foreach ($available_tags as $tag) {
-            $array_for_json[] = [
+        $arrayForJson = [];
+        foreach ($availableTags as $tag) {
+            $arrayForJson[] = [
                 'id' => $tag->id,
                 'name' => $tag->name,
                 'selectable' => $tag->selectable,
                 'children' => $this->availableTagsForJs($tag->children)
             ];
         }
-        return $array_for_json;
+        return $arrayForJson;
     }
-    private function selectedTagsForJs($selected_tags)
+    private function selectedTagsForJs($selectedTags)
     {
-        $array_for_json = [];
-        foreach ($selected_tags as $tag) {
-            $array_for_json[] = [
+        $arrayForJson = [];
+        foreach ($selectedTags as $tag) {
+            $arrayForJson[] = [
                 'id' => $tag->id,
                 'name' => $tag->name
             ];
         }
-        return $array_for_json;
+        return $arrayForJson;
     }
     /**
      * If necessary, convert selected_tags from an array of IDs to a full array of tag info
-     * @param array $selected_tags
+     * @param array $selectedTags
      * @return array
      */
-    private function formatSelectedTags($selected_tags)
+    private function formatSelectedTags($selectedTags)
     {
-        if (empty($selected_tags)) {
+        if (empty($selectedTags)) {
             return [];
         }
-        if (is_array($selected_tags[0])) {
-            return $selected_tags;
+        if (is_array($selectedTags[0])) {
+            return $selectedTags;
         }
         $this->Tags = TableRegistry::get('Tags');
         $retval = [];
 
-        foreach ($selected_tags as $tag_id) {
+        foreach ($selectedTags as $tag_id) {
             $result = $this->Tags->find()
                 ->select(['id', 'name', 'parent_id', 'listed', 'selectable'])
                 ->where(['id' => $tag_id])
                 ->first();
             $retval[] = $result;
         }
+
         return $retval;
     }
-    public function setup($available_tags, $selected_tags = [], $options = [])
+    public function setup($availableTags, $selectedTags = [], $options = [])
     {
-        $params = array(
-            'tags: '.json_encode($this->availableTagsForJs($available_tags))
-        );
-        if (! empty($selected_tags)) {
-            $selected_tags = $this->formatSelectedTags($selected_tags);
-            $params[] = 'selected_tags: '.json_encode($this->selectedTagsForJs($selected_tags));
+        $params = [
+            'tags: ' . json_encode($this->availableTagsForJs($availableTags))
+        ];
+        if (!empty($selectedTags)) {
+            $selectedTags = $this->formatSelectedTags($selectedTags);
+            $params[] = 'selected_tags: ' . json_encode($this->selectedTagsForJs($selectedTags));
         }
         if (! empty($options)) {
             foreach ($options as $var => $val) {
@@ -76,10 +77,6 @@ class TagHelper extends Helper
                 $params[] = "$var: $val";
             }
         }
-        echo '
-            <script>
-                TagManager.init({'.implode(', ', $params).'});
-            </script>
-        ';
+        echo '<script>TagManager.init({' . implode(', ', $params) . '});</script>';
     }
 }
